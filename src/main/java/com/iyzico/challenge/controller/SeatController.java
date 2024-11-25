d0364db620d4b866885189c80b1851a5b2181c26
Mehmet Melikşah Özceyhan <meliksah.ozceyhan@ayvos.com>
2024-11-25 10:24:24 +0300

    feat: unused import statement removed

--- src/main/java/com/iyzico/challenge/controller/SeatController.java
+++ src/main/java/com/iyzico/challenge/controller/SeatController.java
@@ -1,11 +1,8 @@
 package com.iyzico.challenge.controller;
 
-import com.iyzico.challenge.data.dto.FlightRequestDto;
 import com.iyzico.challenge.data.dto.SeatRequestDto;
 import com.iyzico.challenge.data.view.SeatListView;
 import com.iyzico.challenge.data.view.SeatView;
-import com.iyzico.challenge.entity.Flight;
-import com.iyzico.challenge.entity.Seat;
 import com.iyzico.challenge.sdk.api.ApiResponse;
 import com.iyzico.challenge.service.SeatService;
 import org.springframework.data.domain.Page;
@@ -27,33 +24,33 @@ public class SeatController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ApiResponse<SeatView>> getSeat(@PathVariable Long id){
+    public ResponseEntity<ApiResponse<SeatView>> getSeat(@PathVariable Long id) {
         return ResponseEntity.status(HttpStatus.OK)
-                .body(ApiResponse.success(seatService.getSeatById(id),"seat.success"));
+                .body(ApiResponse.success(seatService.getSeatById(id), "seat.success"));
     }
 
     @GetMapping("/flight/{flightId}")
-    public ResponseEntity<ApiResponse<Page<SeatListView>>> getSeatsOfFlight(@PathVariable Long flightId, Pageable pageable){
+    public ResponseEntity<ApiResponse<Page<SeatListView>>> getSeatsOfFlight(@PathVariable Long flightId, Pageable pageable) {
         return ResponseEntity.status(HttpStatus.OK)
-                .body(ApiResponse.success(seatService.getAllSeatsAndPaginate(flightId,pageable),"seat.success"));
+                .body(ApiResponse.success(seatService.getAllSeatsAndPaginate(flightId, pageable), "seat.success"));
     }
 
 
     @PostMapping()
-    public ResponseEntity<ApiResponse<Long>> saveSeat(@RequestBody @Valid SeatRequestDto dto){
+    public ResponseEntity<ApiResponse<Long>> saveSeat(@RequestBody @Valid SeatRequestDto dto) {
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(ApiResponse.success(seatService.saveSeat(dto).getId(),"seat.created"));
+                .body(ApiResponse.success(seatService.saveSeat(dto).getId(), "seat.created"));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ApiResponse<Long>> updateSeat(@PathVariable Long id, @RequestBody @Valid SeatRequestDto dto){
+    public ResponseEntity<ApiResponse<Long>> updateSeat(@PathVariable Long id, @RequestBody @Valid SeatRequestDto dto) {
         return ResponseEntity.status(HttpStatus.OK)
-                .body(ApiResponse.success(seatService.updateSeat(id,dto).getId(),"seat.updated"));
+                .body(ApiResponse.success(seatService.updateSeat(id, dto).getId(), "seat.updated"));
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<ApiResponse<Void>> deleteSeat(@PathVariable Long id){
+    public ResponseEntity<ApiResponse<Void>> deleteSeat(@PathVariable Long id) {
         seatService.deleteSeat(id);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(ApiResponse.success(null,"seat.deleted"));
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(ApiResponse.success(null, "seat.deleted"));
     }
 }
